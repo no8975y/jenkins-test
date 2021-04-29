7db8df6bad77fb163c6e4da615d8b802d0e35296
YU <1303560972@qq.com>
2021-04-29 23:32:08 +0800

    Initial commit

---
+++ src/main/java/com/yu/jenkinstest/TestController.java
@@ -0,0 +1,20 @@
+package com.yu.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ychao
+ * @date 2021/4/29 23:25
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello(){
+        String message = "hello jenkins";
+        return message;
+    }
+
+
+}
