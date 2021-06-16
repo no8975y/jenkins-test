9c218b8080e2a1365f01b601e2d6b0d9919ea731
YU <1303560972@qq.com>
2021-06-16 22:14:39 +0800

    修复war不能在tomcat中启动的bug

--- src/main/java/com/yu/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/yu/jenkinstest/JenkinsTestApplication.java
@@ -2,9 +2,23 @@ package com.yu.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+/**
+ * create by: ychao
+ * description: 打war包是注释掉启动类，放开下面的方法
+ * create time: 2021/6/16 22:13
+ */
+//public class JenkinsTestApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JenkinsTestApplication.class, args);
+//    }
+//
+//}
+
+public class JenkinsTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
